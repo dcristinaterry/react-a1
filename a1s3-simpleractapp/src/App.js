55453d9338a573296828ec1e0be699eb828539e2
dcristinaterry <crisdc88@gmail.com>
2020-09-08 11:29:02 -0500

    add function inputchangeHandler.  get input value from input

--- a1s3-simpleractapp/src/App.js
+++ a1s3-simpleractapp/src/App.js
@@ -5,20 +5,17 @@ import UserInput from './components/UserInput/UserInput'
 
 function App() {
 
-  // const [input, setInput] = useState({ inputValue: "" })
+  const [input, setInput] = useState({ inputValue: "" })
   const [paragraph, setParagraph] = useState({ paragraphValue: "superCristina" })
 
-  // let inputChangeHandler = (event) => {
-  //   setParagraph({ paragraphValue: event.target.value })
-  //   setInput({ inputValue: event.target.value });
-  //   console.log("input value", input.inputValue)
+  let inputChangeHandler = (event) => {
+    setParagraph({ paragraphValue: event.target.value })
+    setInput({ inputValue: event.target.value });
+    console.log("input value", input.inputValue)
 
 
-  // }
+  }
 
-  // const paragraphChange = (event) => {
-  //   setParagraph({ paragraphValue: event.target.value })
-  // }
   return (
     <div className="App">
       <UserInput
