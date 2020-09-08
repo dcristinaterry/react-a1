939d87e279e131b5fdf7cef8cdb3100ba89bd861
dcristinaterry <crisdc88@gmail.com>
2020-09-08 11:22:51 -0500

    add components

---
+++ a1s3-simpleractapp/src/components/UserInput/UserInput.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// const userInput = props => {
+
+//     return (
+//         <div>
+//             <p>Enter text</p>
+//             <input 
+//             type="text" 
+//             onChange={props.change}  
+//             value={props.value}></input>
+            
+//         </div>
+
+//     )
+
+
+
+// }
+
+export default userInput;
---
+++ a1s3-simpleractapp/src/components/UserOutput/UserOupt.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// const userOutput = props => {
+
+//     return(
+//         <div>
+//             <p>Entered text: {props.valuetext}</p>
+//         </div>
+//     );
+
+// }
+
+export default userOutput;
